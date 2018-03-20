aad5e63ebf508a63aeac4cfecfb52c9f3be73e36
공채원 <chaewonkong@gmail.com>
2018-03-20 15:42:57 +0900

    Change templates and css

--- blog/templatetags/markdownify.py
+++ blog/templatetags/markdownify.py
@@ -1,28 +1,28 @@
-"""Markdown lib"""
+"""Markdown View for html blog"""
 
 from django import template
 import mistune
 
 # Syntax Highlighter
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters import HtmlFormatter
+# from pygments import highlight
+# from pygments.lexers import get_lexer_by_name
+# from pygments.formatters import HtmlFormatter
 
 register = template.Library()
 
 
-class HighlightRenderer(mistune.Renderer):
+"""class HighlightRenderer(mistune.Renderer):
 	def block_code(self, code, lang):
 		if not lang:
 			return '\n<pre><code>%s</code></pre>\n' % \
 				mistune.escape(code)
 		lexer = get_lexer_by_name(lang, stripall=True)
 		formatter = HtmlFormatter()
-		return highlight(code, lexer, formatter)
+		return code, lexer, formatter"""
 
 
 @register.filter
 def markdown(value):
-	renderer = HighlightRenderer()
-	markdown = mistune.Markdown(renderer=renderer)
+	#renderer = HighlightRenderer()
+	markdown = mistune.Markdown() #renderer=renderer
 	return markdown(value)
