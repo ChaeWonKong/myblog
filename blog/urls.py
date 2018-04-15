b42cac8944b561c37073d7aa7f30cddfdaa2c224
공채원 <chaewonkong@gmail.com>
2018-04-15 09:28:01 +0900

    Change pk to id in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
 		path('', views.post_list, name='post_list'),
-		path('<int:pk>/', views.post_detail, name='post_detail'),
+		path('<int:id>/', views.post_detail, name='post_detail'),
 		path('post/new/', views.post_new, name='post_new'),
-		path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+		path('post/<int:id>/edit/', views.post_edit, name='post_edit'),
 		path('about', views.about, name='about'),
-		path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+		path('post/<int:id>/remove/', views.post_remove, name='post_remove'),
 		path('<str:category>', views.category_list, name='category_list'),
-		path('<int:pk>/like_action/', views.like_action, name='like_action'),
+		path('<int:id>/like_action/', views.like_action, name='like_action'),
 ]
