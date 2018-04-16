3c7bd9ce645a39d516701e50b5a0efd658e7e172
공채원 <chaewonkong@gmail.com>
2018-04-16 17:09:56 +0900

    Change buttons as default

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-		path('', views.post_list, name='post_list'),
+		path('', views.buttons, name='buttons'),
+		path('index', views.post_list, name='post_list'),
 		path('<int:pk>/', views.post_detail, name='post_detail'),
 		path('post/new/', views.post_new, name='post_new'),
 		path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
@@ -11,6 +12,4 @@ urlpatterns = [
 		path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 		path('<str:category>', views.category_list, name='category_list'),
 		path('<int:pk>/like_action/', views.like_action, name='like_action'),
-		path('buttons/', views.buttons, name="buttons"),
-		path('keyboard', views.buttons, name="keyboard"),
 ]
