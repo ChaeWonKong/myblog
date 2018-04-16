2ffc14ad35255f831d229ae2e5039df4e7b32eea
공채원 <chaewonkong@gmail.com>
2018-04-16 17:11:48 +0900

    url setting

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-		path('', views.buttons, name='buttons'),
-		path('index', views.post_list, name='post_list'),
+		path('keyboard', views.buttons, name='buttons'),
+		path('', views.post_list, name='post_list'),
 		path('<int:pk>/', views.post_detail, name='post_detail'),
 		path('post/new/', views.post_new, name='post_new'),
 		path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
