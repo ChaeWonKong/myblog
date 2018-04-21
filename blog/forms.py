8d33568f2202e19cf7a3f4b758a668e8716622f4
공채원 <chaewonkong@gmail.com>
2018-04-21 14:25:39 +0900

    Add ImageForm class to forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,18 @@
 from django import forms
-from .models import Post
+from .models import Post, PostImage
 
 
 class PostForm(forms.ModelForm):
-
 	class Meta:
 		model = Post
 		fields = ('category', 'title', 'text')
+
+
+class ImageForm(forms.ModelForm):
+	class Meta:
+		model = PostImage
+		fields = ('title', 'img')
+
+	def __init__(self, *args, **kwargs):
+		super(ImageForm, self).__init__(*args, **kwargs)
+		self.fields['img'].required = False
