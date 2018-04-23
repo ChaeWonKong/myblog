a064cd9dca046d5009a6e300539b6d32aa7b1cea
공채원 <chaewonkong@gmail.com>
2018-04-23 17:51:14 +0900

    Edit post_new and post_edit in views

--- blog/views.py
+++ blog/views.py
@@ -83,7 +83,7 @@ def post_detail(request, pk):
 	return render(request, 'blog/post_detail.html', {'post': post})
 
 
-def post_new(request):
+def post_new(request): #
 	if request.method == "POST":
 		form = PostForm(request.POST)
 
@@ -100,7 +100,7 @@ def post_new(request):
 	return render(request, 'blog/post_edit.html', {'form': form})
 
 
-def post_edit(request, pk):
+def post_edit(request, pk): #
 	post = get_object_or_404(Post, pk=pk)
 
 	if request.method == "POST":
