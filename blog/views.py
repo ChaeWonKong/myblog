4d1afd3e17cf581d7ab33d1120a2fe4acac48f1b
공채원 <chaewonkong@gmail.com>
2018-05-24 18:54:45 +0900

    Delete scraper in views

--- blog/views.py
+++ blog/views.py
@@ -1,66 +1,14 @@
 # -*- coding: utf-8 -*-
-import requests
 import json
-from bs4 import BeautifulSoup
 
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 
 from .forms import PostForm, ImageForm
 from .models import Post, PostImage
-
-
-def case_scraper(request):
-	"""Search request in casenote.kr and return outcome"""
-
-	link = "https://casenote.kr/search/?q="+request
-	req = requests.get(link)
-	html = req.text
-	soup = BeautifulSoup(html, 'html.parser')
-	issue = soup.find('div', {'class': 'issue'}).get_text()
-	summary = soup.find('div', {'class': 'summary'}).get_text()
-
-	return [issue, summary, link]
-
-
-def buttons(request):
-	return JsonResponse({
-		"type": "buttons",
-		"buttons": ['판례검색', ]
-		})
-
-
-@csrf_exempt
-def message(request):
-	json_str = (request.body).decode('utf-8')
-	json_data = json.loads(json_str)
-	request = json_data['content']
-
-	if request =='판례검색':
-		return JsonResponse({
-			"message": {"text": "판례번호를 띄어쓰기 없이 입력해주세요"}
-			})
-	else:
-		try:
-			case = case_scraper(request)
-		except:
-			return JsonResponse({
-				"message": {
-				"text": "해당 판례를 찾을 수 없습니다.\n띄어쓰기에 유의해 다시 검색해주세요.\n\n 예) 98다22543"
-				}
-			})
-		else:
-			return JsonResponse({
-				"message": {
-					"text": request + "\n\n판시사항\n\n" + case[0] +
-							"\n\n판결요지\n\n" + case[1] +
-							"\n\n케이스노트에서 자세히 보기\n\n" + case[2]
-					},
-			})
 
 
 def post_list(request):
