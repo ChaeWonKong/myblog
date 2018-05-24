efcf1931907fb4bc0df634a8f470a478ca83e5c5
공채원 <chaewonkong@gmail.com>
2018-05-24 19:03:22 +0900

    Change settings, debug to False

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's(+cxa8oh33r%4e+o#*3)qb^ohw=%0h4t0hw93q3r+l*7mo-cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
 		 'localhost',
