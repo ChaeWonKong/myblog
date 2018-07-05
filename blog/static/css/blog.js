da4c48ecd9fcb9ca0e21df4cf25308966fb33f86
공채원 <chaewonkong@gmail.com>
2018-07-04 22:31:35 -0700

    Fix typo

--- blog/static/css/blog.js
+++ blog/static/css/blog.js
@@ -1,10 +1,4 @@
 // Javascript for Blog
-const js_code = document.getElementById('js_code');
-const py_code = document.getElementById('py_code');
-
-js_code.addEventListener('click', code.codeBlock('javascript'));
-py_code.addEventListener('click', code.codeBlock('python'));
-
 let code = {
 	"text": document.getElementById('id_text'),
 	"codeBlock": function(lang) {
