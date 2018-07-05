f737f6b0d218eb040b962a7c0b8b0de96f419016
Chaewonkong <chaewonkong@gmail.com>
2018-07-05 05:22:13 +0000

    Collect static

--- static/css/blog.js
+++ static/css/blog.js
@@ -1,4 +1,7 @@
 // Javascript for Blog
+const js_code = document.getElementById('js_code');
+const py_code = document.getElementById('py_code');
+
 js_code.addEventListener('click', code.codeBlock('javascript'));
 py_code.addEventListener('click', code.codeBlock('python'));
 
