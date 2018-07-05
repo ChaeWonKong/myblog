e56193c8bc2c435e2c3c6c64b63089ad747387f1
Chaewonkong <chaewonkong@gmail.com>
2018-07-05 05:01:39 +0000

    Collect static

---
+++ static/css/blog.js
@@ -0,0 +1,12 @@
+// Javascript for Blog
+window.onload = function() {
+	js_code.addEventListener('click', code.codeBlock('javascript'));
+	py_code.addEventListener('click', code.codeBlock('python'));
+};
+
+let code = {
+	"text": document.getElementById('id_text'),
+	"codeBlock": function(lang) {
+		this.text.value += '</br><pre><code class="' + lang + '"></code></pre>';
+	}
+}
