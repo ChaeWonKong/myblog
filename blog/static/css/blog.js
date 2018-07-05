9bf9358abd112d92ee6203e3c8cd09504a430715
공채원 <chaewonkong@gmail.com>
2018-07-04 22:52:47 -0700

    Add br button

--- blog/static/css/blog.js
+++ blog/static/css/blog.js
@@ -1,7 +1,10 @@
 // Javascript for Blog
-let code = {
+let editor = {
 	"text": document.getElementById('id_text'),
 	"codeBlock": function(lang) {
 		this.text.value += '</br><pre><code class="' + lang + '"></code></pre>';
+	},
+	"addBr": function() {
+		this.text.value += "<br />";
 	}
 }
