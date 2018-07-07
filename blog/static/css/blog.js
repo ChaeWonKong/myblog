58bb86f2621ebfa097e18fb646eb7f789bed4b88
공채원 <chaewonkong@gmail.com>
2018-07-06 17:40:18 -0700

    change const to var

--- blog/static/css/blog.js
+++ blog/static/css/blog.js
@@ -12,7 +12,7 @@ btn_br.addEventListener('click', function(){
 });
 
 
-let editor = {
+var editor = {
 	"text": document.getElementById('id_text'),
 	"codeBlock": function(lang) {
 		this.text.value += '</br><pre><code class="' + lang + '"></code></pre>';
