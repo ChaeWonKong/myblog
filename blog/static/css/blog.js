5ff2e908e10cb662d3d12f559cb357221f299256
공채원 <chaewonkong@gmail.com>
2018-07-06 17:36:01 -0700

    Change onclick to addEventListener

--- blog/static/css/blog.js
+++ blog/static/css/blog.js
@@ -1,4 +1,17 @@
 // Javascript for Blog
+btn_js.addEventListener('click', function(){
+	editor.codeBlock('javascript');
+});
+
+btn_py.addEventListener('click', function(){
+	editor.codeBlock('python');
+});
+
+btn_br.addEventListener('click', function(){
+	editor.addBr();
+});
+
+
 let editor = {
 	"text": document.getElementById('id_text'),
 	"codeBlock": function(lang) {
