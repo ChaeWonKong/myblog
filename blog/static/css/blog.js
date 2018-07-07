1080593efbef1d0cb6996d6e7acfc9795279639d
공채원 <chaewonkong@gmail.com>
2018-07-06 18:52:17 -0700

    Fix typo in br tag

--- blog/static/css/blog.js
+++ blog/static/css/blog.js
@@ -2,7 +2,7 @@
 const editor = {
 	"text": document.getElementById('id_text'),
 	"codeBlock": function(lang) {
-		this.text.value += '</br><pre><code class="' + lang + '"></code></pre>';
+		this.text.value += '<br /><pre><code class="' + lang + '"></code></pre>';
 	},
 	"addBr": function() {
 		this.text.value += "<br />";
