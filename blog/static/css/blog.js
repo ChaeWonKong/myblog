2b2e2cc28fe2b9c27132cc92d9ab5132e4ed1835
공채원 <chaewonkong@gmail.com>
2018-07-06 17:59:44 -0700

    Remove addEventListener and add onclick

--- blog/static/css/blog.js
+++ blog/static/css/blog.js
@@ -1,18 +1,5 @@
-// Javascript for Blog
-btn_js.addEventListener('click', function(){
-	editor.codeBlock('javascript');
-});
 
-btn_py.addEventListener('click', function(){
-	editor.codeBlock('python');
-});
-
-btn_br.addEventListener('click', function(){
-	editor.addBr();
-});
-
-
-var editor = {
+const editor = {
 	"text": document.getElementById('id_text'),
 	"codeBlock": function(lang) {
 		this.text.value += '</br><pre><code class="' + lang + '"></code></pre>';
