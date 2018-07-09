9b15748345e2964400e2db40386360cfbc60e63b
Chaewonkong <chaewonkong@gmail.com>
2018-07-09 15:35:29 +0000

    Collect static

--- static/css/blog.js
+++ static/css/blog.js
@@ -2,11 +2,21 @@
 const editor = {
 	"text": document.getElementById('id_text'),
 	"highlight": window.getSelection(),
+	"cursorPos": this.text.selectionStart,
+	"beforeStr": this.text.value.substring(0, cursorPos),
+	"afterStr": this.text.value.substring(cursorPos, text.value.length),
 	"codeBlock": function(lang) {
-		this.text.value += '<br /><pre><code class="' + lang + '"></code></pre>';
+		this.text.value = 
+			this.beforeStr 
+			+ '<br /><pre><code class="' 
+			+ lang + '"></code></pre>' 
+			+ this.afterStr;
 	},
 	"addBr": function() {
-		this.text.value += "<br />";
+		this.text.value = 
+			this.beforeStr
+			+ "<br />"
+			+ this.afterStr;
 	},
 	"makeBolder": function() {
 		this.highlight = '<b>' + this.highlight + '</b>';
