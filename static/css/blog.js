408c73c4cd16ab7595c87d635f1de40c2fc17023
Chaewonkong <chaewonkong@gmail.com>
2018-07-09 15:11:16 +0000

    Collect static

--- static/css/blog.js
+++ static/css/blog.js
@@ -1,10 +1,15 @@
 
 const editor = {
 	"text": document.getElementById('id_text'),
+	"highlight": window.getSelection(),
 	"codeBlock": function(lang) {
 		this.text.value += '<br /><pre><code class="' + lang + '"></code></pre>';
 	},
 	"addBr": function() {
 		this.text.value += "<br />";
+	},
+	"makeBolder": function() {
+		this.highlight = '<b>' + highlight + '</b>';
+		this.text.value = this.text.value.replace(window.getSelection(), this.highlight);
 	}
 };
