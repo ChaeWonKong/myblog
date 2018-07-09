68244fee81122dc1041bc75599d31974c51cffba
Chaewonkong <chaewonkong@gmail.com>
2018-07-09 15:39:50 +0000

    Collect static

--- static/css/blog.js
+++ static/css/blog.js
@@ -3,9 +3,12 @@ const editor = {
 	"text": document.getElementById('id_text'),
 	"highlight": window.getSelection(),
 	"cursorPos": this.text.selectionStart,
-	"beforeStr": this.text.value.substring(0, cursorPos),
-	"afterStr": this.text.value.substring(cursorPos, text.value.length),
+	"beforeStr": this.text.value.substring(0, this.cursorPos),
+	"afterStr": this.text.value.substring(this.cursorPos, this.text.value.length),
 	"codeBlock": function(lang) {
+		// const cursorPos = this.text.selectionStart;
+		// const beforeStr = this.text.value.substring(0, cursorPos);
+		// const afterStr = this.text.value.substring(cursorpos, this.text.value.length);
 		this.text.value = 
 			this.beforeStr 
 			+ '<br /><pre><code class="' 
