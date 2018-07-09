97285b0d42f17365389ca7b94295eeadea6e66de
Chaewonkong <chaewonkong@gmail.com>
2018-07-09 15:43:11 +0000

    Collect static

--- static/css/blog.js
+++ static/css/blog.js
@@ -2,24 +2,24 @@
 const editor = {
 	"text": document.getElementById('id_text'),
 	"highlight": window.getSelection(),
-	"cursorPos": this.text.selectionStart,
-	"beforeStr": this.text.value.substring(0, this.cursorPos),
-	"afterStr": this.text.value.substring(this.cursorPos, this.text.value.length),
 	"codeBlock": function(lang) {
-		// const cursorPos = this.text.selectionStart;
-		// const beforeStr = this.text.value.substring(0, cursorPos);
-		// const afterStr = this.text.value.substring(cursorpos, this.text.value.length);
+		const cursorPos = this.text.selectionStart;
+		const beforeStr = this.text.value.substring(0, cursorPos);
+		const afterStr = this.text.value.substring(cursorpos, this.text.value.length);
 		this.text.value = 
-			this.beforeStr 
+			beforeStr 
 			+ '<br /><pre><code class="' 
 			+ lang + '"></code></pre>' 
-			+ this.afterStr;
+			+ afterStr;
 	},
 	"addBr": function() {
+		const cursorPos = this.text.selectionStart;
+		const beforeStr = this.text.value.substring(0, cursorPos);
+		const afterStr = this.text.value.substring(cursorpos, this.text.value.length);
 		this.text.value = 
-			this.beforeStr
+			beforeStr
 			+ "<br />"
-			+ this.afterStr;
+			+ afterStr;
 	},
 	"makeBolder": function() {
 		this.highlight = '<b>' + this.highlight + '</b>';
